70e621c00712819000336158eee67ee25d61e308
lei <794227403@qq.com>
2024-08-28 15:55:21 +0800

    load_buffer

--- vre_dmc/src/load_buffer.py
+++ vre_dmc/src/load_buffer.py
@@ -1,18 +1,100 @@
 import utils
 import torch
 import os
+import sys
+sys.path.append(os.getcwd())
 import numpy as np
 import PIL as Image
 import matplotlib.pyplot as plt
+import random
+import seaborn as sns
+import pandas as pd
+from augmentations import random_conv, random_overlay, random_shift
+from umap import UMAP
+from resnet_model_test import resnet_model
 
-replay_buffer = torch.load('saved_buffers/replay_buffer_500000.pt')
-test_buffer_1 = torch.load('saved_buffers/test_buffer_1_500000.pt')
-test_buffer_2 = torch.load('saved_buffers/test_buffer_2_500000.pt')
-test_buffer_3 = torch.load('saved_buffers/test_buffer_3_500000.pt')
 
-plt.imshow(np.transpose(test_buffer_3._obses[00000][1].frames[0],(1,2,0)))
-plt.savefig('test_buffer_3_frame_0.png')
+def main():
+    replay_buffer = torch.load('saved_buffers/replay_buffer_500000.pt')
+    test_buffer_1 = torch.load('saved_buffers/test_buffer_1_500000.pt')
+    test_buffer_2 = torch.load('saved_buffers/test_buffer_2_500000.pt')
+    test_buffer_3 = torch.load('saved_buffers/test_buffer_3_500000.pt')
+    # RL更新所用的frame是9*84*84，一次采了三张图片
+    batch_size = 1000
+    obses_rb = buffer_sample_obs(replay_buffer, batch_size)
+    obses_tb1 = buffer_sample_obs(test_buffer_1, batch_size)
+    obses_tb2 = buffer_sample_obs(test_buffer_2, batch_size)
+    obses_tb3 = buffer_sample_obs(test_buffer_3, batch_size)
 
-# TODO: T-SNE or UMDP
+    # np.moveaxis(array,0,-1)    # 将array的第0维移动维最后的维度
+    rb_tensor = torch.cat([torch.tensor([(np.array(obses_rb[i][0]).reshape(3,3,84,84)) for i in range(batch_size)]).reshape(-1,3,84,84)
+                          ,torch.tensor([(np.array(obses_rb[i][1]).reshape(3,3,84,84)) for i in range(batch_size)]).reshape(-1,3,84,84)],dim=0).float().to('cuda:1')
+    tb1_tensor = torch.cat([torch.tensor([(np.array(obses_tb1[i][0]).reshape(3,3,84,84)) for i in range(batch_size)]).reshape(-1,3,84,84)
+                          ,torch.tensor([(np.array(obses_tb1[i][1]).reshape(3,3,84,84)) for i in range(batch_size)]).reshape(-1,3,84,84)],dim=0).float().to('cuda:1')
+    tb2_tensor = torch.cat([torch.tensor([(np.array(obses_tb2[i][0]).reshape(3,3,84,84)) for i in range(batch_size)]).reshape(-1,3,84,84)
+                          ,torch.tensor([(np.array(obses_tb2[i][1]).reshape(3,3,84,84)) for i in range(batch_size)]).reshape(-1,3,84,84)],dim=0).float().to('cuda:1')
+    tb3_tensor = torch.cat([torch.tensor([(np.array(obses_tb3[i][0]).reshape(3,3,84,84)) for i in range(batch_size)]).reshape(-1,3,84,84)
+                          ,torch.tensor([(np.array(obses_tb3[i][1]).reshape(3,3,84,84)) for i in range(batch_size)]).reshape(-1,3,84,84)],dim=0).float().to('cuda:1')
 
-# TODO: ResNet50 for encoder
+    # ---------use augmentations ---------
+    with torch.no_grad():
+        rb_shift = random_shift(rb_tensor)
+        rb_conv = random_conv(rb_tensor)
+        rb_over = random_overlay(rb_tensor)
+
+    # ---------resnet&umap----------------
+    RS = resnet_model('resnet50')
+    umap_model = UMAP(n_components=2, n_neighbors=15, min_dist=0.1, densmap = True, dens_lambda = 2)
+
+    with torch.no_grad():
+        out_rb = RS.resnet_model(rb_tensor)
+        out_tb1 = RS.resnet_model(tb1_tensor)
+        out_tb2 = RS.resnet_model(tb2_tensor)
+        out_tb3 = RS.resnet_model(tb3_tensor)
+    
+    length = out_rb.shape[0]
+    all_out = torch.cat([out_rb,out_tb1,out_tb2,out_tb3],dim=0).cpu()
+    # all_out = np.vstack([out_rb.cpu().numpy(),out_tb1.cpu().numpy(),out_tb2.cpu().numpy(),out_tb3.cpu().numpy()])    # for np.array
+    scatters = umap_model.fit_transform(all_out)
+
+    # ------------plot--------------------
+    df = pd.DataFrame(scatters, columns=["x", "y"])
+    df ["label"] = None
+    df ["label"][:length] = "train_state"
+    df ["label"][length:2*length] = "color_hard"
+    df ["label"][2*length:3*length] = "video_easy"
+    df ["label"][3*length:] = "video_hard"
+
+    palette = sns.color_palette("Set1",5)
+    sns.set_palette(palette)
+    label_mode = ["color_hard","video_easy","video_hard"]
+    palette=['#DC565F', '#3E61AC']
+
+    for i in range(3):       
+        df_d = pd.concat([df[df["label"] == "train_state"],df[df["label"] == label_mode[i]]], ignore_index=True)
+        df_d.to_csv("encoding.csv")
+        plt.figure()
+        legend = True
+        sns.scatterplot(data=df_d, x="x", y="y", hue="label", style='label',sizes='label', s=20 , palette=palette, legend = legend)
+        
+        plt.savefig(f"train--{label_mode[i]}.png", dpi=300)
+        plt.close()
+
+
+    frame = np.transpose(test_buffer_3._obses[0][1].frames[0],(1,2,0))
+    plt.imshow(frame)
+    plt.imsave('test_buffer_3_frame_0.png', frame)
+
+
+
+
+    # TODO: T-SNE or UMDP
+
+def buffer_sample_obs(buffer: object, batch_size: int = 1000):
+    length= len(buffer._obses)
+    idx = random.sample(range(length), batch_size)
+    obses_samples =[buffer._obses[i] for i in idx]
+    return obses_samples
+
+if __name__ == '__main__':
+    main()
